03f57acc1635a4abee2f17e05786b7559d6b7dae
Khalid Zoabi <kzoabi@outlook.com>
2026-03-25 03:11:23 +0000

    Adding arg parser, text/json output modes, and the "types" command

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import packageJson from '../package.json'
+import * as types from './types'
+
+import { parseArgs } from 'node:util'
 
 export async function cli (params: { args: string[] }) {
-  const [command, ...rest] = params.args
+  const { positionals: [ command, ...rest ], values } = parseArgs({
+    args: params.args,
+    strict: true,
+    allowPositionals: true,
+    options: {
+      json: { type: 'boolean' }
+    }
+  })
   if (command === 'version' && rest.length === 0) {
-    console.info(packageJson.version)
+    if (values.json) console.info({ version: packageJson.version })
+    else console.info(packageJson.version)
+  } else if (command === 'types' && rest.length === 0) {
+    if (values.json) console.info(Object.keys(types))
+    else console.info(Object.keys(types).map(type => `- ${type}`).join('\n'))
   } else {
-    console.error(`Error: Invalid command or arguments.`)
+    if (values.json) console.error({ error: 'Invalid command or arguments.' })
+    else console.error(`Error: Invalid command or arguments.`)
     process.exitCode = 1
-  }  
+  }
 }
 
 if (import.meta.main) {
