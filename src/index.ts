2c8d11e296322ac606c50c435a6c23f6ac88f04f
Khalid Zoabi <kzoabi@outlook.com>
2026-03-25 02:54:33 +0000

    Refactoring the CLI to delegate to the bun implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import packageJson from '../package.json'
+
+export async function cli (params: { args: string[] }) {
+  const [command, ...rest] = params.args
+  if (command === 'version' && rest.length === 0) {
+    console.info(packageJson.version)
+  } else {
+    console.error(`Error: Invalid command or arguments.`)
+    process.exitCode = 1
+  }  
+}
+
+if (import.meta.main) {
+  cli({ args: process.argv.slice(2) })
+}
