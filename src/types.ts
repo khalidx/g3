ac0610f3567eb235ec0879ac6d39871e19d4d0d7
Khalid Zoabi <kzoabi@outlook.com>
2026-03-31 01:53:52 +0000

    Adding the CommandSchema type

--- src/types.ts
+++ src/types.ts
@@ -3,12 +3,23 @@ import { z } from 'zod'
 const NonEmptyStringSchema = z.string().min(1)
 const LocalPathSchema = NonEmptyStringSchema.startsWith('./')
 
+export type External = { target: string, source: Externals[string] }
 export type Externals = z.infer<typeof ExternalsSchema>
 export const ExternalsSchema = z.record(
   LocalPathSchema,
   z.object({
-    path: LocalPathSchema,
     repo: NonEmptyStringSchema,
+    path: LocalPathSchema,
     ref: NonEmptyStringSchema
   })
 )
+
+export type Command = z.infer<typeof CommandSchema>
+export const CommandSchema = z.object({
+  name: NonEmptyStringSchema,
+  handler: (
+    z.function()
+    .input([ z.object({ values: z.object({ json: z.boolean() }) }) ])
+    .output(z.promise(z.void()))
+  )
+})
